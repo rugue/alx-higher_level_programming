f88ce77d6e1d247b1995640cf0c3d06103f161fb
Osarugue Enehizena <nehirugue@gmail.com>
2023-06-09 03:50:13 -0700

    calculator added

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My adding funct
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """My subtracting function
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """My multiplying funct
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """My dividing funct
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
