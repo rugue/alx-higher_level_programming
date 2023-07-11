ed0f11cc98e3134d19d735a562c246c15233de85
Osarugue Enehizena <nehirugue@gmail.com>
2023-07-11 05:46:26 -0700

    inheritane

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Contains lookup function
+"""
+
+
+def lookup(obj):
+    """returns list of available attributes and methods of object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Contains class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
